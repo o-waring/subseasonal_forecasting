8dd6d02e6bbcf2c3276893efd7b42eaf7c1d6a49
Ollie <oliver.waring@qbizuk.com>
2020-07-24 15:21:19 +0100

    Only download 2019 if files don't already exist

--- weather_forecasting_app/download/get_feature_data_2019.sh
+++ weather_forecasting_app/download/get_feature_data_2019.sh
@@ -1,26 +1,55 @@
 # height 2019
-cd ../data/prediction_inputs/ && { curl -O ftp://ftp.cdc.noaa.gov/Datasets/ncep.reanalysis.dailyavgs/pressure/hgt.2019.nc -o hgt.2019.nc ; cd -; }
-
-# # icec 2019 #WRONG - need PCA data
-# cd ../../data/prediction_inputs/ && { curl -O ftp://ftp.cdc.noaa.gov/Projects/Datasets/noaa.oisst.v2.highres/icec.day.mean.2019.nc -o icec.day.mean.2019.nc ; cd -; }
-#
-# # sst 2019 #WRONG - need PCA data
-# cd ../../data/prediction_inputs/ && { curl -O ftp://ftp.cdc.noaa.gov/Projects/Datasets/noaa.oisst.v2.highres/sst.day.mean.2019.nc -o sst.day.mean.2019.nc ; cd -; }
+FILE=data/prediction_inputs/hgt.2019.nc
+if [ -f "$FILE" ]; then
+    echo "$FILE exists."
+else
+  cd data/prediction_inputs/ && { curl -O ftp://ftp.cdc.noaa.gov/Datasets/ncep.reanalysis.dailyavgs/pressure/hgt.2019.nc -o hgt.2019.nc ; cd -; }
+fi
 
 # rhum at surface 2019
-cd ../data/prediction_inputs/ && { curl -O ftp://ftp.cdc.noaa.gov/Datasets/ncep.reanalysis.dailyavgs/surface/rhum.sig995.2019.nc -o rhum.sig995.2019.nc ; cd -; }
+FILE=data/prediction_inputs/rhum.sig995.2019.nc
+if [ -f "$FILE" ]; then
+    echo "$FILE exists."
+else
+  cd data/prediction_inputs/ && { curl -O ftp://ftp.cdc.noaa.gov/Datasets/ncep.reanalysis.dailyavgs/surface/rhum.sig995.2019.nc -o rhum.sig995.2019.nc ; cd -; }
+fi
 
 # pevpr 2019
-cd ../data/prediction_inputs/ && { curl -O ftp://ftp.cdc.noaa.gov/Datasets/ncep.reanalysis/surface_gauss/pevpr.sfc.gauss.2019.nc -o pevpr.sfc.gauss.2019.nc ; cd -; }
+FILE=data/prediction_inputs/pevpr.sfc.gauss.2019.nc
+if [ -f "$FILE" ]; then
+    echo "$FILE exists."
+else
+  cd data/prediction_inputs/ && { curl -O ftp://ftp.cdc.noaa.gov/Datasets/ncep.reanalysis/surface_gauss/pevpr.sfc.gauss.2019.nc -o pevpr.sfc.gauss.2019.nc ; cd -; }
+fi
 
 # pr_wtr 2020 precipitable water
-cd ../data/prediction_inputs/ && { curl -O ftp://ftp.cdc.noaa.gov/Datasets/ncep.reanalysis/surface/pr_wtr.eatm.2019.nc -o pr_wtr.eatm.2019.nc ; cd -; }
+FILE=data/prediction_inputs/pr_wtr.eatm.2019.nc
+if [ -f "$FILE" ]; then
+    echo "$FILE exists."
+else
+  cd data/prediction_inputs/ && { curl -O ftp://ftp.cdc.noaa.gov/Datasets/ncep.reanalysis/surface/pr_wtr.eatm.2019.nc -o pr_wtr.eatm.2019.nc ; cd -; }
+fi
 
 # pres gauss 2019
-cd ../data/prediction_inputs/ && { curl -O ftp://ftp.cdc.noaa.gov/Datasets/ncep.reanalysis/surface_gauss/pres.sfc.gauss.2019.nc -o pres.sfc.gauss.2019.nc ; cd -; }
+FILE=data/prediction_inputs/pres.sfc.gauss.2019.nc
+if [ -f "$FILE" ]; then
+    echo "$FILE exists."
+else
+  cd data/prediction_inputs/ && { curl -O ftp://ftp.cdc.noaa.gov/Datasets/ncep.reanalysis/surface_gauss/pres.sfc.gauss.2019.nc -o pres.sfc.gauss.2019.nc ; cd -; }
+fi
 
 # Elevation
-cd ../data/prediction_inputs/ && { curl -O http://research.jisao.washington.edu/data_sets/elevation/elev.1-deg.nc -o elev.1-deg.nc ; cd -; }
+FILE=data/prediction_inputs/elev.1-deg.nc
+if [ -f "$FILE" ]; then
+    echo "$FILE exists."
+else
+  cd data/prediction_inputs/ && { curl -O http://research.jisao.washington.edu/data_sets/elevation/elev.1-deg.nc -o elev.1-deg.nc ; cd -; }
+fi
 
 # sea level pressure 2019
-cd ../data/prediction_inputs/ && { curl -O ftp://ftp.cdc.noaa.gov/Datasets/ncep.reanalysis/surface/slp.2019.nc -o slp.2019.nc ; cd -; }
+FILE=data/prediction_inputs/slp.2019.nc
+if [ -f "$FILE" ]; then
+    echo "$FILE exists."
+else
+  cd data/prediction_inputs/ && { curl -O ftp://ftp.cdc.noaa.gov/Datasets/ncep.reanalysis/surface/slp.2019.nc -o slp.2019.nc ; cd -; }
+fi

fb0b05cfe759db37feeb80cc3a830e800a58f3d9
Ollie <oliver.waring@qbizuk.com>
2020-07-24 10:39:50 +0100

    Add test file for testing refactor functions

---
+++ weather_forecasting_app/test.py
@@ -0,0 +1,21 @@
+import joblib
+from glob import glob
+import numpy as np
+import pandas as pd
+import sklearn
+
+from src.get_data.load_functions import load_tar_datasets, load_column_names, load_feature_datasets, load_locations, load_standardizers
+
+if __name__ == "__main__":
+
+    prec_df = load_tar_datasets(pred_var='temp34', file_dir='data/')
+
+    locs = load_locations()
+
+    col_names = load_column_names(features_only=True)
+
+    # feat_df = load_feature_datasets() #TODO
+
+    feature_means, feature_stds, feature_scaler, prec_scaler, tmp_scaler = load_standardizers()
+
+    print('\n',feature_means,'\n', feature_stds,'\n', feature_scaler,'\n', prec_scaler,'\n', tmp_scaler)

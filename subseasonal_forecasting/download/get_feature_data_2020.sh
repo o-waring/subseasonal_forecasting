e99f2007268453473fe1b49cc1c8ee7d48c2eb6b
Ollie <oliver.waring@qbizuk.com>
2020-08-06 17:34:59 +0100

    Edit path names in get_feature_data_2020

--- subseasonal_forecasting/download/get_feature_data_2020.sh
+++ subseasonal_forecasting/download/get_feature_data_2020.sh
@@ -2,12 +2,12 @@
 cd data/prediction_inputs/ && { curl -O ftp://ftp.cdc.noaa.gov/Datasets/ncep.reanalysis.dailyavgs/pressure/hgt.2020.nc -o hgt.2020.nc ; cd -; }
 
 # # icec 2020 #WRONG - need PCA data
-# cd data/prediction_inputs/ && { curl -O ftp://ftp.cdc.noaa.gov/Projects/Datasets/noaa.oisst.v2.highres/icec.day.mean.2020.nc -o icec.day.mean.2020.nc ; cd -; }
+# cd data/prediction_inputs_raw/ && { curl -O ftp://ftp.cdc.noaa.gov/Projects/Datasets/noaa.oisst.v2.highres/icec.day.mean.2020.nc -o icec.day.mean.2020.nc ; cd -; }
 # Perhaps - ftp://ftp.cdc.noaa.gov/Datasets/ncep.reanalysis/surface_gauss/
 
 #
 # # sst 2020 #WRONG - need PCA data
-# cd data/prediction_inputs/ && { curl -O ftp://ftp.cdc.noaa.gov/Projects/Datasets/noaa.oisst.v2.highres/sst.day.mean.2020.nc -o sst.day.mean.2020.nc ; cd -; }
+# cd data/prediction_inputs_raw/ && { curl -O ftp://ftp.cdc.noaa.gov/Projects/Datasets/noaa.oisst.v2.highres/sst.day.mean.2020.nc -o sst.day.mean.2020.nc ; cd -; }
 
 # rhum at surface 2019
 cd data/prediction_inputs/ && { curl -O ftp://ftp.cdc.noaa.gov/Datasets/ncep.reanalysis.dailyavgs/surface/rhum.sig995.2020.nc -o rhum.sig995.2020.nc ; cd -; }
@@ -25,4 +25,4 @@ cd data/prediction_inputs/ && { curl -O ftp://ftp.cdc.noaa.gov/Datasets/ncep.rea
 cd data/prediction_inputs/ && { curl -O ftp://ftp.cdc.noaa.gov/Datasets/ncep.reanalysis/surface/slp.2020.nc -o slp.2020.nc ; cd -; }
 
 # MEI
-#cd data/prediction_inputs/ && { curl -O https://psl.noaa.gov/enso/mei/data/meiv2.data -o meiv2.data ; cd -; }
+#cd data/prediction_inputs_raw/ && { curl -O https://psl.noaa.gov/enso/mei/data/meiv2.data -o meiv2.data ; cd -; }
